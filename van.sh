c2de908da909fbf70a5a87411f516ef12c7bcf66
van125 <57973628+van125@users.noreply.github.com>
2019-11-20 09:35:58 +0700

    Add files via upload

---
+++ van.sh
@@ -0,0 +1,41 @@
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+echo
+toilet -f big -F gay VANDARK
+echo $green"UCAPKAN SALAM TOLOL"
+echo
+sleep 2
+
+echo $cyan"CEPET PILIH JGN BANYAK BACOD :"
+echo
+echo $blue"[1]" $yellow"Stabilkan Jaringan"
+echo
+echo $blue"[0]" $yellow"Keluar"
+echo
+echo $red
+read -p "PILIH YANG MANA SAYANG : " bro
+
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+clear
+toilet -f big -F gay VAN
+echo $green"STABILKAN JARINGAN BY VAN BIAR GAK LEMOT"
+sleep 2
+ping -s1000 1.1.1.1
+fi
+
+if [ $bro = 0 ] || [ $bro = 0 ]
+then
+clear
+echo $cyan
+figlet "VANDARK"
+echo $yellow"ASHIAPP"
+sleep 3
+exit
+fi
